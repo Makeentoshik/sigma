089287e70bb7c8c4ca41d0fab73fa021074ca0b3
Magzhan Zhakupov <magazhakupov@gmail.com>
2020-11-22 18:47:08 +0600

    intital commit

---
+++ vue-cli/src/pages/Example.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="wrapper-content wrapper-content--fixed">
+  <section>
+<div class="container">
+<h1>Example Page</h1>
+<input type="text" v-model="message">
+<button class="btn btnPrimary" @click="setMessage">Change message</button>
+<br>
+<br>
+
+<p> {{ getMessage }}</p>
+
+ 
+</div>
+  </section>
+  </div>
+
+</template>
+
